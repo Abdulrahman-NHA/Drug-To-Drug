3fea1fc09bccf5f4658e5716628803c4fc9d8aa3
Abdurahman-NHA <abdulrahmannabeelA@gmail.com>
2024-12-12 19:04:54 +0300

    pc server updateded

--- src/components/DrugList.js
+++ src/components/DrugList.js
@@ -1,39 +1,44 @@
-// src/components/DrugList.js
-
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Card from './Card';
 import Text from './Text';
-// Removed TitleText import and any TitleText usage inside
 
-const DrugList = ({ drugs, addDrug }) => {
-  return (
-    <div className="drug-list">
-      <Card>
-        {/* Remove or comment out TitleText line here if it existed */}
-        {/* <TitleText text="Search Results" /> */}
-        {drugs.length === 0 ? (
-          <p>No results found. Try a different search term.</p>
-        ) : (
-          <ul>
-            {drugs.map((drug) => (
-              <li key={drug.drug_id} className="small-card">
-                <Text text={drug.name} className="drug-name" />
-                <div>
-                  <Link to={`/drugs/${drug.drug_id}`} className="button-small details-button">
-                    Details
-                  </Link>
-                  <button className="button-small add-button" onClick={() => addDrug(drug)}>
-                    Add
-                  </button>
-                </div>
-              </li>
-            ))}
-          </ul>
-        )}
-      </Card>
-    </div>
-  );
+const DrugList = ({ drugs = [], addDrug = () => console.warn("addDrug not provided") }) => {
+    if (!Array.isArray(drugs)) {
+        console.error("Drugs is not an array:", drugs);
+        return <p>Error: Unable to display drug list.</p>;
+    }
+
+    return (
+        <div className="drug-list">
+            <Card>
+                {drugs.length === 0 ? (
+                    <div>
+                        <p>No results found. Try a different search term.</p>
+                    </div>
+                ) : (
+                    <ul>
+                        {drugs.map((drug) => (
+                            <li key={drug.drug_id} className="small-card">
+                                <Text text={drug.name} className="drug-name" />
+                                <div>
+                                    <Link to={`/drugs/${drug.drug_id}`} className="button-small details-button">
+                                        Details
+                                    </Link>
+                                    <button
+                                        className="button-small add-button"
+                                        onClick={() => addDrug(drug)}
+                                    >
+                                        Add
+                                    </button>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                )}
+            </Card>
+        </div>
+    );
 };
 
 export default DrugList;

449d132821f28366ef7c2fb75618e24ac705ff43
Abdurahman-NHA <abdulrahmannabeelA@gmail.com>
2024-12-12 21:15:42 +0300

    pc server updateded

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,12 +1,14 @@
-import axios from "axios";
+// src/api/axiosConfig.js
+
+import axios from 'axios';
 
 const getCookie = (name) => {
   let cookieValue = null;
-  if (document.cookie) {
+  if (document.cookie && document.cookie !== "") {
     const cookies = document.cookie.split(";");
     for (let cookie of cookies) {
       cookie = cookie.trim();
-      if (cookie.startsWith(`${name}=`)) {
+      if (cookie.startsWith(name + "=")) {
         cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
         break;
       }
@@ -16,12 +18,12 @@ const getCookie = (name) => {
 };
 
 const axiosInstance = axios.create({
-  baseURL: "http://66.118.132.73:8000/api",  // Replace with your No-IP address
-  withCredentials: true, 
+  baseURL: 'https://ddrug-interaction-eb3a91a13ab1.herokuapp.com/api', // Heroku API base URL
+  withCredentials: true, // Important for sending cookies
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
+    'X-CSRFToken': getCookie('csrftoken'),
   },
 });
-
 
 export default axiosInstance;

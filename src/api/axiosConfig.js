2709c2f5162c5c0650b77d17771700e6d44024f8
Abdurahman-NHA <abdulrahmannabeelA@gmail.com>
2024-12-14 23:29:38 +0300

    Updated Axiosi

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://ddrug-interaction-eb3a91a13ab1.herokuapp.com/api/',
+  baseURL: `${process.env.REACT_APP_API_BASE_URL}api/`,
   headers: {
     'Content-Type': 'application/json',
   },

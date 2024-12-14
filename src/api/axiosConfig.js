2e74a399fdeca1b61f17ae9deae580617abd3703
Abdurahman-NHA <abdulrahmannabeelA@gmail.com>
2024-12-14 22:58:31 +0300

    Updated Axiosi

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,26 +1,9 @@
 import axios from 'axios';
 
-const getCookie = (name) => {
-  let cookieValue = null;
-  if (document.cookie && document.cookie !== '') {
-    const cookies = document.cookie.split(';');
-    for (let cookie of cookies) {
-      cookie = cookie.trim();
-      if (cookie.startsWith(name + '=')) {
-        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-        break;
-      }
-    }
-  }
-  return cookieValue;
-};
-
 const axiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/', // Change this to your local backend URL
-  withCredentials: true, // Important for sending cookies
+  baseURL: 'https://ddrug-interaction-eb3a91a13ab1.herokuapp.com/api/',
   headers: {
     'Content-Type': 'application/json',
-    'X-CSRFToken': getCookie('csrftoken'),
   },
 });
 

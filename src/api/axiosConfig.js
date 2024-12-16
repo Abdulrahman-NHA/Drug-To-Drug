4f0039a4eb33dcfad6a8054bc972f3e452598a04
Abdurahman-NHA <abdulrahmannabeelA@gmail.com>
2024-12-16 20:17:18 +0300

    pc server updateded

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const baseURL = process.env.REACT_APP_API_BASE_URL || "https://ddrug-intteraction-e80de02fc9c1.herokuapp.com/api";
+const baseURL = process.env.REACT_APP_API_BASE_URL || "https://ddrug-intteraction-e80de02fc9c1.herokuapp.com";
 
 const axiosInstance = axios.create({
-  baseURL,  // Use baseURL directly
+  baseURL: baseURL, // Automatically uses the Heroku URL
   headers: {
     'Content-Type': 'application/json',
   },
